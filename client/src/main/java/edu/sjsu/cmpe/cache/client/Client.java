9113a8dbc66a9403fa4f428c4adc1d900de3eaa2
poojaindi <pooja.indi@gmail.com>
2014-04-30 12:49:49 -0700

    added for loop

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -32,77 +32,17 @@ public class Client {
 //        consistentMap.add(cache3);
         
         String value;
+        String str[] = {"x", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+        int i;
         
-        dummyCache = consistentMap.get(1);
-        dummyCache.put(1, "a");
-        value = dummyCache.get(1);
-        System.out.println("get(1) => " + value);
-        
-        dummyCache = consistentMap.get(2);
-        dummyCache.put(2, "b");
-        value = dummyCache.get(2);
-        System.out.println("get(2) => " + value);
-        
-        dummyCache = consistentMap.get(3);
-        dummyCache.put(3, "c");
-        value = dummyCache.get(3);
-        System.out.println("get(3) => " + value);
-        
-        dummyCache = consistentMap.get(4);
-        dummyCache.put(4, "d");
-        value = dummyCache.get(4);
-        System.out.println("get(4) => " + value);
-        
-        dummyCache = consistentMap.get(5);
-        dummyCache.put(5, "e");
-        value = dummyCache.get(5);
-        System.out.println("get(5) => " + value);
-        
-        dummyCache = consistentMap.get(6);
-        dummyCache.put(6, "f");
-        value = dummyCache.get(6);
-        System.out.println("get(6) => " + value);
-        
-        dummyCache = consistentMap.get(7);
-        dummyCache.put(7, "g");
-        value = dummyCache.get(7);
-        System.out.println("get(7) => " + value);
-        
-        dummyCache = consistentMap.get(8);
-        dummyCache.put(8, "h");
-        value = dummyCache.get(8);
-        System.out.println("get(8) => " + value);
-        
-        dummyCache = consistentMap.get(9);
-        dummyCache.put(9, "i");
-        value = dummyCache.get(9);
-        System.out.println("get(9) => " + value);
-        
-        dummyCache = consistentMap.get(10);
-        dummyCache.put(10, "j");
-        value = dummyCache.get(10);
-        System.out.println("get(10) => " + value);
-        
-    //   String value = cache1.get(1);
-    //   System.out.println("get(1) => " + value);
-       
-       
-        
-        int size = cacheList.size();
-        
- /*       int k = 0;
-        for(int i =0 ; i < 10; i++){
-        	int bucket = Hashing.consistentHash(Hashing.md5().hashInt(i),size);
-        	System.out.println(bucket);
+        for(i = 1 ; i <= 10 ; i++){
         	
-        }*/
+        	dummyCache = consistentMap.get(i);
+            dummyCache.put(i, str[i]);
+            value = dummyCache.get(i);
+            System.out.println("get(i) => " + value);
+        }
         
-        
-       
-
-       // cache1.put(1, "foo");
-       // System.out.println("put(1 => foo)");
-
 
         System.out.println("Existing Cache Client..."); 
     }
